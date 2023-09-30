7598628796eef464aca2651c19ae28b7c66db213
abdelmadjidbettache <b.madjidprof@hotmail.fr>
2023-09-29 20:32:51 -0400

    first commit

---
+++ LesDelegate/Calcul.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesDelegate
+{
+    internal class Calcul
+    {
+        public delegate int DelegateCalcul(int a, int b);
+        public static int Somme(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Produit(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int DemoCalcul(int a, int b)
+        {
+            DelegateCalcul delCal = Somme;
+            return delCal(a, b);
+            delCal = Produit; 
+            return delCal(a, b);
+
+        }
+
+
+    }
+}
---
+++ LesDelegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesDelegate
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int result = 0;
+            
+
+            Console.WriteLine("Premier nb svp:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Deuxieme nb svp:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Quelle operation '+' ou '*': ");
+            char choix = (char)Console.Read();
+            switch (choix)
+            {
+                case '+':
+                    { result = Calcul.Somme(n1, n2); break; }
+                case '*':
+                    { result = Calcul.Produit(n1, n2); break; }
+            }
+            Console.WriteLine("Resultat: "+result.ToString());
+
+            new Calcul().DemoCalcul(n1, n2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
